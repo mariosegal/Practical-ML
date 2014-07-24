6b12e5ebcf0fc1f9b9934f30eee40967648e585d
Mario Segal <mario.segal@gmail.com>
2014-07-24 11:50:22 -0400

    added quiz 3

--- Class Project.R
+++ Class Project.R
@@ -3,6 +3,7 @@
 
 #Load Libraries
 require(caret)
+require(rattle)
 
 #1 Read Data
 
@@ -45,7 +46,8 @@ full_vars <- which(sapply(train,function(x) sum(is.na(x)))==0)
 full_vars <- full_vars[-(1:7)]
 
 train_full <- subset(train,,full_vars)
-train_full <- train_full[-c(1:7)]
+nearZeroVar(train_full)  #good they have no NAs and have proper variance
+
 groups <- c("_belt","_arm","_dumbbell","_forearm")
 #may be highly correlated, first let's check that
 
@@ -60,7 +62,7 @@ which(M > 0.8,arr.ind=T)
 M <- abs(cor(train_full[which(grepl(groups[2],names(train_full)))]))
 diag(M) <- 0
 which(M > 0.8,arr.ind=T)
-#I see some  correlation for belt
+#I see some  correlation for arm
 
 M <- abs(cor(train_full[which(grepl(groups[3],names(train_full)))]))
 diag(M) <- 0
@@ -73,40 +75,63 @@ diag(M) <- 0
 which(M > 0.8,arr.ind=T)
 #I see very littel for forearm
 
-
+#create pc's for each sensor
 pc_belt <- preProcess(train_full[which(grepl(groups[1],names(train_full)))],method="pca")
 pc_belt1 <- predict(pc_belt,train_full[which(grepl(groups[1],names(train_full)))])
-names(pc_belt1) <- paste(names(pc_belt1),groups[1],sep="_")
+names(pc_belt1) <- tolower(paste(names(pc_belt1),groups[1],sep=""))
 
 pc_arm <- preProcess(train_full[which(grepl(groups[2],names(train_full)))],method="pca")
 pc_arm1 <- predict(pc_arm,train_full[which(grepl(groups[2],names(train_full)))])
-names(pc_arm1) <- paste(names(pc_arm1),groups[2],sep="_")
+names(pc_arm1) <- tolower(paste(names(pc_arm1),groups[2],sep=""))
 
 pc_dumbbell <- preProcess(train_full[which(grepl(groups[3],names(train_full)))],method="pca")
 pc_dumbbell1 <- predict(pc_dumbbell,train_full[which(grepl(groups[3],names(train_full)))])
-names(pc_dumbbell1) <- paste(names(pc_dumbbell3),groups[3],sep="_")
+names(pc_dumbbell1) <- tolower(paste(names(pc_dumbbell1),groups[3],sep=""))
 
 pc_forearm <- preProcess(train_full[which(grepl(groups[4],names(train_full)))],method="pca")
 pc_forearm1 <- predict(pc_forearm,train_full[which(grepl(groups[4],names(train_full)))])
-names(pc_forearm1) <- paste(names(pc_forearm4),groups[4],sep="_")
+names(pc_forearm1) <- tolower(paste(names(pc_forearm1),groups[4],sep=""))
+
+#visualize the PCs
+ggplot(pc_arm1,aes(x=pc1_arm,y=pc2_arm,color=train_full$classe))+geom_point()
+ggplot(pc_dumbbell1,aes(x=pc1_dumbbell,y=pc2_dumbbell,color=train_full$classe))+geom_point()+coord_cartesian(ylim=c(0,10))
+ggplot(pc_belt1,aes(x=pc1_belt,y=pc2_belt,color=train_full$classe))+geom_point()
+ggplot(pc_forearm1,aes(x=pc1_forearm,y=pc2_forearm,color=train_full$classe))+geom_point()+coord_cartesian(ylim=c(-5,5))
 
 
+#do the same for test;
+test_pc_arm <- predict(pc_arm,newdata=test[row.names(pc_arm$rotation)])
+names(test_pc_arm) <- tolower(paste(names(test_pc_arm),"_arm",sep=""))
+test_pc_forearm <- predict(pc_forearm,newdata=test[row.names(pc_forearm$rotation)])
+names(test_pc_forearm) <- tolower(paste(names(test_pc_forearm),"_forearm",sep=""))
+test_pc_dumbbell <- predict(pc_dumbbell,newdata=test[row.names(pc_dumbbell$rotation)])
+names(test_pc_dumbbell) <- tolower(paste(names(test_pc_dumbbell),"_dumbbell",sep=""))
+test_pc_belt <- predict(pc_belt,newdata=test[row.names(pc_belt$rotation)])
+names(test_pc_belt) <- tolower(paste(names(test_pc_belt),"_belt",sep=""))
 
+test_pc <- data.frame(cbind(test_pc_arm,test_pc_belt,test_pc_dumbbell,test_pc_forearm))
+
+#combine all the PCs into an analysis set
+train_pc <- data.frame(cbind(pc_belt1,pc_arm1,pc_dumbbell1,pc_forearm1),classe=train_full$classe)
+
+#I also want to try PCs across all variables
+pc_all <- preProcess(train_full,method="pca")
+pc_all1 <- predict(pc_forearm,train_full])
+names(pc_all1) <- tolower(paste(names(pc_all1),"all",sep="_"))
 
 #####Analysis ####
-sum(complete.cases(train[c(8:159)]))   #There are no complete cases, so we can't just work with full data
-sapply(train[c(8:159)])
+sum(complete.cases(train_pc)) 
 
-#I will have to drop variables or impute
+#Always a good idea to start with a tree
+model1 <- train(classe~.,data=train_pc,method="rpart") 
+model1$finalModel   
+fancyRpartPlot(model1$finalModel) #this did not work at all as it is not predicting C or E
 
-#I think I have to scale the data first - then do the analysis
-#it also seems the missing data is a problem, so I would likely have to impute it
+#Lets try a more complicated approach - Random Forest that is more robust
+model2 <- train(classe~.,data=train_pc,method="rf") 
+model2
+confusionMatrix(model2)
+results2 <- data.frame(actual=train_pc$classe,predicted=predict(model2))
+ggplot(results2,aes(x=actual,y=predicted,color=actual))+geom_jitter(size=0.5,alpha=0.5)+theme_bw()+theme(legend.position="none")
 
-#try SVD to reduce the variables
-svd1 <- svd(scale(train[,c(8:159)]))
-
-#It does not seem to me I will be able to identify them by sight, but clearly the arm measures are the most 
-#discrinant by sight, and the charts are kind of cool
-
-# I think I need to run some PCA on this or other technique to idetify the top variables
-
+test2 <- data.frame(actual=train_pc$classe,predicted=predict(model2))
